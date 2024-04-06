e845c2ee4135cccafcd4d401222a137e6945cae5
dotRarufu <81721522+dotRarufu@users.noreply.github.com>
2024-04-06 23:16:51 +0800

    static config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,17 +6,11 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-  adapter: adapter({
-    // if true, will create a Netlify Edge Function rather
-    // than using standard Node-based functions
-    edge: false,
-
-    // if true, will split your app into multiple functions
-    // instead of creating a single one for the entire app.
-    // if `edge` is true, this option cannot be used
-    split: false,
-  }),
+  kit: {
+    adapter: adapter({
+      fallback: "index.html", // may differ from host to host
+    }),
+  },
 };
 
 export default config;
